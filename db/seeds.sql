6621fee3d0024e626bf4997751d7d46332c1889d
Garlic Fiend <suzannaakins@gmail.com>
2020-11-12 19:54:26 -0800

    seeds/schema files, seeding data

---
+++ db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO cocktails (title)
+VALUES
+  ('Old Fashioned'),
+  ('Cosmopolitan'),
+  ('Margarita'),
+  ('Martini');
+
+INSERT INTO juices (title)
+VALUES
+  ('lime'),
+  ('cranberry'),
+  ('lemon'),
+  ('orange');
+
+INSERT INTO sweet (title)
+VALUES
+  ('simple syrup'),
+  ('agave'),
+  ('honey'),
+  ('strawberry simple syrup');
+
+  INSERT INTO wine (title)
+  VALUES
+    ('cabernet'),
+    ('merlot'),
+    ('chardonnay');
