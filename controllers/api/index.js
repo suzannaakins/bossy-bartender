5cb485042b5eb18777cf8f0e997a5578e40d113c
Amanda Guerriero <ag.codingclass@gmail.com>
2020-11-13 15:53:15 -0800

    initial theming & handlebar setup

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,7 +5,10 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 
 router.use('/users', userRoutes);
+// router.use('/drinks', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+
+
 
 module.exports = router;
