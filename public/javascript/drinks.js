6730f7ed68f448af38df6aef9c41cf56375b4f63
Amanda Guerriero <ag.codingclass@gmail.com>
2020-11-15 13:25:52 -0800

    Added get recipe and starting on print recipe functions

--- public/javascript/drinks.js
+++ public/javascript/drinks.js
@@ -1,109 +1,99 @@
-const ingArr = [];
-const drinkSave1 = [];
-var introContainerEl = $("#drink-container");
+// const ingArr = [];
+// const drinkSave = [];
+var drinkContainerEl = $("#drink-container");
 
 // Get Drinks from Cocktails DB by each ingredient
 function getDrinksByIngList(event) {
     // Need Ingredients indicated by user
-    var ing = ingArr
+    // var ing = ingArr
     fetch(
-        ('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Lime_Juice')
+        ('https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=Gin,Sugar,Lime_Juice')
     )
     .then(function(drinkResponse) {
         return drinkResponse.json();
     })
     .then(function(drinkReponse) {
-        createDrinksArray(drinkReponse);
-        // loopDrinkMatches(drinkReponse);
+        printDrinkOptions(drinkReponse);
         console.log(drinkReponse);
     });
 };
 
-var createDrinksArray = function (response) {
-    console.log ("I'm clicked")
+// Print the drink results to the user
+var printDrinkOptions = function (response) {
+    console.log(response.drinks.length)
     // Loop through the drinks
     for (let i =0; i < response.drinks.length; i++) {
-        console.log(response.drinks.length)
-        
-            var drinkTitle = response.drinks[i].strDrink;
-            var drinkImage = response.drinks[i].strDrinkThumb;
-            var drinkId = response.drinks[i].idDrink;
-        
-            if (drinkTitle !== "") {
+        // Container for Each Drink
+        var drinkCardContainer = $("<div>").addClass("col-sm");
+        var card = $("<div>").addClass("card");
+        var image = $("<div>").addClass("card-image-top");
+        var drinkId = response.drinks[i].idDrink
+        // Display each Drink
+        var drinkImage = $("<img>")
+            .attr("src", response.drinks[i].strDrinkThumb)
+        var drinkTitle = $("<h5>")
+            .addClass("card-title")
+            .text(response.drinks[i].strDrink);
+        var drinkButton = $("<button>")
+            .addClass("btn-sm")
+            .attr("id", "drink-click-" + [i])
+            .text("Click Me");
 
-            var drinkInfo = {
-                title: drinkTitle,
-                image: drinkImage,
-                id: drinkId
-            };
-             if (.indexOf(drinkInfo) == -1){
-            // Add the value to the array
-             drinkSave.push(drinkInfo);
-            }
-            console.log (ingArr1st);
-        }
+        $(document).on("click", "#drink-click-" + [i], function() {
+            console.log(drinkId)
+            getRecipe(drinkId)
+        });
+
+        // Append Display to Container
+        card.append(image.append(drinkImage));
+        card.append(drinkTitle, drinkButton);
+        drinkCardContainer.append(card);
+        drinkContainerEl.append(drinkCardContainer);
     }
+};
+
+// On Search 
+$(document).on('click', '.search-button', function() {
+    getDrinksByIngList();
+});
+    
+
+// Once clicked call the cocktail API by Drink & Display the Recipe
+function getRecipe(id) {
+    console.log("I'm clicked")
+    // Need to pass in a variable for user's choice
+    fetch(
+        ('https://www.thecocktaildb.com/api/json/v2/9973533/lookup.php?i='+ id)
+    )
+    .then(function(recipeResponse) {
+        return recipeResponse.json();
+    })
+    .then(function(recipeResponse) {
+        printRecipe(recipeResponse)
+        console.log(recipeResponse);
+    });
+};
+
+function printRecipe(response) {
+    console.log("Recipe");
 }
 
-    //Example:
-    // Get list of Gin Drinks
-    // https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin
-    // Then get list of Lime Juice
-    // https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Lime%20Juice
-     
-    // Create Array 1 of Objects & Array 2 Objects & PossibleDrinks 
-    
-    // Possibly loop through Array 1 & loop through Array 2 - compare and push out the ones that are equal
-    // let possibleDrinks =
+    // var saveArray = funciton() {
+        //  var drinkTitle = response.drinks[i].strDrink;
+        //  var drinkImage = response.drinks[i].strDrinkThumb;
+        //  var drinkId = response.drinks[i].idDrink;
+        
+        //  if (drinkTitle !== "") {
 
-    // Print the drink results to the user
-    // var loopDrinkMatches = function (drinkMatch) {
-    //     // Loop through the drink
-    //     console.log("I'm Clicked")
-    //     for (let i =0; i < drinkMatch.length; i++) {
-      
-    //       // Container for Each Drink
-    //       var drinkCardContainer = $("<div>").addClass("col-sm");
-    //       var card = $("<div>").addClass("card");
-    //       var image = $("<div>").addClass("card-image-top");
-    //       var title = $("<div>").addClass("card-title");
-      
-      
-    //       // Display each Drink
-    //       var drinkImage = $("<img>")
-    //         .attr("src", response.drinks[i].strDrinkThumb)
-    //       var drinkTitle = $("<h5>")
-    //         .text(response.drinks[i].strDrink);
-    //       var drinkChoose = $("<a>")
-    //         console.log("I'm choosen")
-      
-    //       // Append Display to Container
-    //       card.append(image.append(drinkImage));
-    //       card.append(body.append(drinkTitle));
-    //       drinkCardContainer.append(card);
-    //       drinkContainerEl.append(drinkCardContainer);
-    //     }
-    //   };
-
-    
-    // On Search 
-    $(document).on('click', '.search-button', function() {
-        getDrinksByIngList();
-        // printCocktails();
-    })
-    
-
-    // Once clicked call the cocktail API by Drink & Display the Recipe
-    function displayChoosenDrink() {
-        // Need to pass in a variable for user's choice
-        fetch(
-            ('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007')
-        )
-        .then(function(drinkResponse) {
-            return drinkResponse.json();
-        })
-        .then(function(drinkReponse) {
-            createDrinksArray(drinkReponse);
-            console.log(drinkReponse);
-        });
-    };
+        //  var drinkInfo = {
+        //      title: drinkTitle,
+        //      image: drinkImage,
+        //      id: drinkId
+        //  };
+        //   if (drinkSave.indexOf(drinkInfo) == -1){
+        //  Add the value to the array
+        //   drinkSave.push(drinkInfo);
+        //  }
+        //  console.log (drinkSave);
+        // }
+// }
