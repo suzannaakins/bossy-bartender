bb633e3e6ce833c2f09e3fa27ba83978ab2ce0e4
Amanda Guerriero <ag.codingclass@gmail.com>
2020-11-17 20:48:27 -0800

    Sent text dynamically

--- controllers/api/twilio-routes.js
+++ controllers/api/twilio-routes.js
@@ -5,21 +5,13 @@ const client = require('twilio')(accountSid, authToken);
 
 const router = require('express').Router();
 
-router.post("/", (req,res) => {
+router.post('/', (req,res) => {
     client.messages.create({
-        body: 'Hello - test from Node',
-        to: '+13105692944',  // Text this number
+        to: req.body.phone_number,
+        body: req.body.recipe,
         from: '+13157549675' // From a valid Twilio number
     })
     .then((message) => console.log(message.sid));
 })
-
-// client.messages
-//   .create({
-//      body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-//      from: '+15017122661',
-//      to: '+15558675310'
-//    })
-//   .then(message => console.log(message.sid));
 
 module.exports = router;
--- public/javascript/sms.js
+++ public/javascript/sms.js
@@ -1,8 +1,28 @@
-function sendText() {
-    fetch('http://localhost:3002/api/twilio', {
-        method: 'POST'
+async function sendText(event) {
+    event.preventDefault();
+  
+    const phone_number = document.querySelector('.phone').value;
+    console.log(phone_number)
+    const recipe = "Recipe";
+  
+    if (phone_number) {
+        const response = await fetch('/api/twilio', {
+        method: 'POST',
+        body: JSON.stringify({
+            phone_number,
+            recipe,
+          }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
-};
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
 
 // Query Selector & Fetch
 document.querySelector('#smsText').addEventListener('click', sendText);
