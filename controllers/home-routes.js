2671fa0f4c7a391a7799f94161300a61c2de465f
Garlic Fiend <suzannaakins@gmail.com>
2020-11-17 21:18:52 -0800

    dropdown menus complete

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,19 +4,44 @@ const { Ingredient } = require('../models');
 
 router.get('/', (req, res) => {
     Ingredient.findAll({
-        where: {
-            category_id: 1
-        },
         attributes: [
-            'name'
+            'name',
+            'category_id'
         ]
     })
         .then(data => {
-            const spirits = data.map(spirit => spirit.get({ plain: true }));
+            const spirits = data.filter(spirit => spirit.category_id === 1);
+            const filteredSpirits = spirits.map(filteredSpirit => filteredSpirit.get({ plain: true }));
+            const wines = data.filter(wine => wine.category_id === 2);
+            const filteredWines = wines.map(filteredWine => filteredWine.get({ plain: true }));
+            const liqueurs = data.filter(liqueur => liqueur.category_id === 3);
+            const filteredLiqueurs = liqueurs.map(filteredLiqueur => filteredLiqueur.get({ plain: true }));
+            const syrups = data.filter(syrup => syrup.category_id === 4);
+            const filteredSyrups = syrups.map(filteredSyrup => filteredSyrup.get({ plain: true }));
+            const foods = data.filter(food => food.category_id === 5);
+            const filteredFoods = foods.map(filteredFood => filteredFood.get({ plain: true }));
+            const juices = data.filter(juice => juice.category_id === 6);
+            const filteredJuices = juices.map(filteredJuice => filteredJuice.get({ plain: true }));
+            const others = data.filter(other => other.category_id === 7);
+            const filteredOthers = juices.map(filteredOther => filteredOther.get({ plain: true }));
+
+            console.log(spirits[0].name);
             // pass a single post object into the homepage template
-            console.log(spirits);
             res.render('homepage', {
-                spirits
+                spirits,
+                filteredSpirits,
+                wines,
+                filteredWines,
+                liqueurs,
+                filteredLiqueurs,
+                syrups,
+                filteredSyrups,
+                foods,
+                filteredFoods,
+                juices,
+                filteredJuices,
+                others,
+                filteredOthers
             });
         })
         .catch(err => {
