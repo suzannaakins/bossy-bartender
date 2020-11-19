a6f979953e5a26f7b6bc9a1962a67d6aab7de6a1
Garlic Fiend <suzannaakins@gmail.com>
2020-11-19 14:10:10 -0800

    category seeds

---
+++ seeds/category-seeds.js
@@ -0,0 +1,37 @@
+const sequelize = require('../config/connection');
+const { Category } = require('../models');
+
+const catdata = [
+  {
+    title: 'Spirits',
+    id: 1
+  },
+  {
+    title: 'Wines/Beer',
+    id: 2
+  },
+  {
+    title: 'Liqueurs',
+    id: 3
+  },
+  {
+    title: 'Syrups',
+    id: 4
+  },
+  {
+    title: 'Food/Fruits',
+    id: 5
+  },
+  {
+    title: 'Juices/Sodas',
+    id: 6
+  },
+  {
+    title: 'Other',
+    id: 7
+  }
+];
+
+const seedCategories = () => Category.bulkCreate(catdata, { individualHooks: true });
+
+module.exports = seedCategories;
--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,6 @@
 const seedUsers = require('./user-seeds');
+const seedIngredients = require('./ingredient-seeds');
+const seedCategories = require('./category-seeds');
 // const seedPosts = require('./post-seeds');
 // const seedComments = require('./comment-seeds');
 // const seedVotes = require('./vote-seeds');
@@ -8,6 +10,13 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('--------------');
+
+  await seedIngredients();
+  console.log('--------------');
+
+  await seedCategories();
+  console.log('--------------');
+
   await seedUsers();
   console.log('--------------');
 
