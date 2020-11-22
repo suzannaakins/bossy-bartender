b1fa4e64db5ceb9b6fb76e3056db468522465e4c
Garlic Fiend <suzannaakins@gmail.com>
2020-11-21 18:25:26 -0800

    trash icons

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -1,21 +1,30 @@
 const userIngredients = [];
+const trashIcon = `<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg>`
+
 //const userIngredients = ['water'];  USE THIS if we get fancier api search working
 document.getElementById("bar").innerHTML = ''
+
 
 $(".dropdown-menu").on('click', '.dropdown-item', function () {
     const ingredient = $(this).text();
     userIngredients.indexOf(ingredient) === -1 ? userIngredients.push(ingredient) : console.log('this item already exists');
-    console.log(userIngredients);
     const bar = document.getElementById("bar")
     const ul = document.createElement("ul")
     bar.textContent = ""
     for (i = 0; i < userIngredients.length; i++) {
         const li = document.createElement("li")
-        li.textContent = userIngredients[i] ;
+        li.innerHTML = userIngredients[i] + "    " + trashIcon;
         ul.append(li)
     }
     bar.append(ul);
-    console.log(userIngredients);
+
+    $('.bi-trash').on('click', function () {
+        console.log('hi')
+    })
+
     // On Search 
     $(document).on('click', '.search-button', function () {
         sendIngredients(userIngredients);
